9afba99273472a09e2ba36d0d858453b57a38363
Cecília Gontijo <ceciliagontijo50@gmail.com>
2026-03-13 11:47:28 -0300

    solving naming problems

---
+++ src/RecyclyServer.API/Entities/Materials.cs
@@ -0,0 +1,9 @@
+﻿namespace RecyclyServer.API.Entities
+{
+    public class Materials
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public Guid CollectionPointId { get; set; }
+    }
+}
